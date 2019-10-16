72e05a3e2be6d70a3cf7b32e254edd4357ec91fc
Aliaksei Balabushka <aliaksei.balabushka@gmail.com>
2019-10-16 20:54:40 +0200

    Change Book schema

--- src/models/book.js
+++ src/models/book.js
@@ -1,24 +1,63 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const schema = new mongoose.Schema({
-  title: {
-    type: String, required: true
+const schema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    authors: {
+      type: String,
+      required: true
+    },
+    image_url: {
+      type: String,
+      required: true
+    },
+    goodreadsId: {
+      type: String,
+      required: true
+    },
+    pages: {
+      type: Number,
+      required: true
+    },
+    average_rating: {
+      type: Number,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    format: {
+      type: String,
+      required: true
+    },
+    publication_day: {
+      type: Number,
+      required: true
+    },
+    publication_month: {
+      type: Number,
+      required: true
+    },
+    publication_year: {
+      type: Number,
+      required: true
+    },
+    numberOfEntities: {
+      type: Number,
+      default: 1,
+      required: true
+    },
+    likeCounter: {
+      type: Number,
+      default: 0,
+      required: true
+    }
   },
-  authors: {
-    type: String, required: true
-  },
-  image_url: {
-    type: String, required: true
-  },
-  goodreadsId: {
-    type: String
-  },
-  pages: {
-    type: Number, required: true
-  },
-  numberOfEntities: {
-    type: Number, default: 1
-  }
-});
+  { versionKey: false }
+);
 
-export default mongoose.model('Book', schema);
+export default mongoose.model("Book", schema);
